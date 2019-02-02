8cb0528d701a993375ad59a35ec5c6d79a13192e
Sez <wingapoh2@MacBook-Pro-di-Sez.local>
2019-02-02 00:04:54 +0000

    30012019

---
+++ ToDoList/Untitled-1.js
@@ -0,0 +1,9 @@
+let numersToBeOrdered = [10, 2, 5, 7, 1, 3];
+function orderNumbers(array) {
+    let newArray = "";
+    for (i = 0; i < array.length; i++) {
+
+    }
+}
+
+console.log(orderNumbers(numersToBeOrdered));
